216b55d50fbdbb27da51888c0ecbc567294370d0
Fares <49195418+FaresCherif@users.noreply.github.com>
2020-11-13 00:08:06 +0100

    Add files via upload

---
+++ Project/Date.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+
+
+using namespace std;
+
+class Date
+{
+	int jour;
+	int mois;
+	int annee;
+
+	int heure;
+	int minute;
+public:
+	Date();
+	~Date();
+};
+
