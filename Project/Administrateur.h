6cf10a478b0f4e605f989378717b0f2f75550ee6
Fares <49195418+FaresCherif@users.noreply.github.com>
2020-11-13 00:27:04 +0100

    Add files via upload

---
+++ Project/Administrateur.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Administrateur
+{
+	string identifiant;
+	string motDePasse;
+public:
+	Administrateur();
+	~Administrateur();
+};
+
