36ea894ceaf3959f0212aa882eca54aa7ef7afbd
Fares <49195418+FaresCherif@users.noreply.github.com>
2020-11-13 01:13:16 +0100

    Add files via upload

--- Project/Source.cpp
+++ Project/Source.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
 #include <windows.h>
+#include <fstream>
+#include <string>
+
 
 using namespace std;
 
 int main() {
-	cout << "test";
-	Sleep(8000);
+	int choix = 0;
+
+	while (choix != 5) {
+		string admin = "n";
+		cout << "etes vous admin ? (y/n)";
+		cin >> admin;
+		
+		if (admin == "y") {
+			choix = 0;
+		}
+		else{
+			if (admin == "n") {
+				choix = 1;
+			}
+			else {
+				choix = 2;
+			}
+		}
+
+
+		switch (choix) {
+		case 0:; break;
+		case 1:; break;
+		case 2: cout<<"Le symbole rentre n est pas accepte"<<"\n"; break;
+
+		}
+
+
+
+	}
+
 }
