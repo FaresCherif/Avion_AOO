e5f56e076df373290b134d648cf6ab8cc5063fdb
Fares <49195418+FaresCherif@users.noreply.github.com>
2020-11-15 02:23:18 +0100

    Add files via upload

--- Project/Source.cpp
+++ Project/Source.cpp
@@ -6,17 +6,65 @@
 
 using namespace std;
 
+void menu() {
+	cout << "             =====  Choix  ====="<<"\n";
+	cout << "1  -- Modifier la Date d un vol" << "\n";
+	cout << "2  -- Modifier l heure d un vol" << "\n";
+	cout << "3  -- Ajouter vol" << "\n";
+	cout << "4  -- Afficher liste des vols" << "\n";
+	cout << "5  -- Quitter et savegarder" << "\n";
+	cout << "6  -- Quitter sans savegarder" << "\n";
+}
+
+
 int main() {
 	int choix = 0;
 
-	while (choix != 5) {
+	while (choix != 6) {
 		string admin = "n";
 		cout << "etes vous admin ? (y/n)";
 		cin >> admin;
 		
 		if (admin == "y") {
 			choix = 0;
+			ifstream fichier;
+			fichier.open("Administrateur.txt", ios::in);
+
+			string ligne;
+
+			while (fichier)    //Tant qu'on n'est pas a la fin
+			{
+				getline(fichier, ligne); //On lit une ligne
+			}
+			string mdp;
+			cout << "Tapez le mot de passe Administrateur : ";
+			cin >> mdp;
+
+			if (mdp == ligne) {
+				cout << "vous etes connecte"<<"\n";
+				int choixMenu = 0;
+
+
+				while (choixMenu!=5 && choixMenu!=6) {
+					menu();
+					cin >> choixMenu;
+					switch (choixMenu)
+					{
+					case 6:
+						choixMenu = 6;
+						choix = 6;
+						break;
+
+					default:
+						cout << "Le symbole entre n est pas accepte" << "\n";
+						break;
+					}
+				}
+			}
+
 		}
+
+		
 		else{
 			if (admin == "n") {
 				choix = 1;
@@ -28,10 +76,10 @@ int main() {
 
 
 		switch (choix) {
-		case 0:; break;
-		case 1:; break;
-		case 2: cout<<"Le symbole rentre n est pas accepte"<<"\n"; break;
-
+			case 0:; break;
+			case 1:; break;
+			case 2: cout<<"Le symbole rentre n est pas accepte"<<"\n"; break;
+			case 6: cout << "Au revoir" << "\n";
 		}
 
 
